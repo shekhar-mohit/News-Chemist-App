76b9727b97ac3d5f50ef059ca987752e9218d838
Mohit Shekhar <mohitshekhar782@gmail.com>
2024-03-20 23:05:39 +0530

    News-Chemist-App

---
+++ app/src/main/java/com/shekharmohit/newschemist/NewsFullActivity.java
@@ -0,0 +1,50 @@
+package com.shekharmohit.newschemist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class NewsFullActivity extends AppCompatActivity {
+    WebView webView;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_full);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        String url = getIntent().getStringExtra("url");
+        webView = findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+    }
+    public void onBackPressed(){
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+
+
+    }
+}
